a6d9964aa5b9270c31563c3c70eb61662901a05a
pandeypowar <118781288+pandeypowar@users.noreply.github.com>
2022-12-07 16:42:29 -0800

    Create price.js

---
+++ working/price.js
@@ -0,0 +1,20 @@
+var date = new Date();
+date.setHours(0, 0, 0, 0);
+var value = 100;
+
+function generateData() {
+  value = Math.round((Math.random() * 10 - 4.2) + value);
+  am5.time.add(date, "day", 1);
+  return {
+    date: date.getTime(),
+    value: value
+  };
+}
+
+function generateDatas(count) {
+  var data = [];
+  for (var i = 0; i < count; ++i) {
+    data.push(generateData());
+  }
+  return data;
+}
